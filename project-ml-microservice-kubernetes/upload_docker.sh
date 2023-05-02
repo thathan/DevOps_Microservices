7018e05abe6e8fa6543868df102c92080ac362b4
Thodoris Athanaileas <todorath@gmail.com>
2023-05-02 21:18:56 +0000

    Task 4: Add script for uploading docker image

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=thathan/project-api
+version=1.0
 
 # Step 2:  
 # Authenticate & tag
+docker login
 echo "Docker ID and Image: $dockerpath"
+docker tag project-api ${dockerpath}:${version}
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}:${version}

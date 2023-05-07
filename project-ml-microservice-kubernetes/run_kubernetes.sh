415608d81d859dbf36771f38878f8af97c8edcd9
Thodoris Athanaileas <todorath@gmail.com>
2023-05-07 13:47:31 +0300

    Task 6: Deploy with Kubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=thathan/project-api:1.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run predictor --image=${dockerpath}
 
 # Step 3:
 # List kubernetes pods
+sleep 5
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward predictor 8000:80
